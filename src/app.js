50511c82629dfa9cfb55d3cea1c63c526701f456
WarrenQuan <quanwarren11@gmail.com>
2022-12-13 15:01:06 -0500

    commented code on adding model

--- src/app.js
+++ src/app.js
@@ -27,7 +27,7 @@ import {
 } from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { SeedScene } from 'scenes';
-
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 
 // Set up the scene, camera, and renderer
 const scene = new SeedScene();
@@ -49,17 +49,43 @@ camera.position.set(0, -50, 140); // angled to like legend of zelda
 // can use lookAt to follow player position too
 camera.lookAt(0, 0, 0);
 
-   // -------- SOUND CODE PT 1 START ---------- //
-    // create an AudioListener and add it to the camera
-    const listener = new AudioListener();
-    camera.add(listener);
+// -------- SOUND CODE PT 1 START ---------- //
+// create an AudioListener and add it to the camera
+const listener = new AudioListener();
+camera.add(listener);
 
-    // create a global audio source
-    const sound = new Audio(listener);
-     // -------- SOUND CODE PT 1 END ---------- //
+// create a global audio source
+const sound = new Audio(listener);
+// -------- SOUND CODE PT 1 END ---------- //
 // -------- PLAYER CODE AND MESH ---------- //
 const playerSize = 40;
 const playerHeight = 100;
+// const loader = new GLTFLoader();
+// // load a resource
+// loader.load(
+//     // resource URL
+//     'src/assets/Chibi_hp.gltf',
+//     // called when resource is loaded
+//     function (object) {
+//         console.log(object.scene.position)
+//         object.scene.scale.set(100, 100, 100); 
+//         console.log(object.scene)
+//         console.log( object.rotation)
+//         scene.add(object.scene);
+//         // if (object) {
+//         //     object.rotation.x += 20;
+//         // }
+//     },
+//     // called when loading is in progresses
+//     function (xhr) {
+//         console.log((xhr.loaded / xhr.total) * 100 + '% loaded');
+//     },
+//     // called when loading has errors
+//     function (error) {
+//         console.log('An error happened', error);
+//     }
+// );
+
 var playerGeometry = new BoxGeometry(playerSize, playerSize, playerHeight);
 var playerMaterial = new MeshBasicMaterial({
     color: 0xff0000,
@@ -129,21 +155,20 @@ for (var i = -3; i < 4; i++) {
 const speed = playerSize / 2;
 function onKeyDown(event) {
     // ----- SOUND PT 2 ----- //
-   // load a sound and set it as the Audio object's buffer
-   // chrome requires user input to start audio so need to be on a key pressed or smth
-   const audioLoader = new AudioLoader();
-   // replace music
-   audioLoader.load(
-       'src/sounds/Morning-Routine-Lofi-Study-Music.mp3',
-       function (buffer) {
-           sound.setBuffer(buffer);
-           sound.setLoop(true);
-           sound.setVolume(0.1);
-           if(!sound.isPlaying)
-           sound.play();
-       }
-   );
-   // ----- SOUND PT 2 ----- //
+    // load a sound and set it as the Audio object's buffer
+    // chrome requires user input to start audio so need to be on a key pressed or smth
+    const audioLoader = new AudioLoader();
+    // replace music
+    audioLoader.load(
+        'src/sounds/Morning-Routine-Lofi-Study-Music.mp3',
+        function (buffer) {
+            sound.setBuffer(buffer);
+            sound.setLoop(true);
+            sound.setVolume(0.1);
+            if (!sound.isPlaying) sound.play();
+        }
+    );
+    // ----- SOUND PT 2 ----- //
     console.log('PLAYER', player.boundingBox);
     console.log('ROOM', room.boundingBox);
     console.log(window.innerWidth - 20, window.innerHeight - 20);
@@ -248,8 +273,16 @@ document.body.style.margin = 0; // Removes margin around page
 document.body.style.overflow = 'hidden'; // Fix scrolling
 document.body.appendChild(canvas);
 
+// const controls = new OrbitControls(camera, canvas);
+// controls.enableDamping = true;
+// controls.enablePan = false;
+// controls.minDistance = 4;
+// controls.maxDistance = 16;
+// controls.update();
+
 // Render loop
 const onAnimationFrameHandler = (timeStamp) => {
+    // controls.update();
     renderer.render(scene, camera);
     scene.update && scene.update(timeStamp);
     window.requestAnimationFrame(onAnimationFrameHandler);
